380d96d1afd51a6b6900fd0f37f4477c202b23ed
Serhii Trush <serhii.trush@eleks.com>
2018-10-18 19:13:27 +0300

    WIP: Added test Silo

---
+++ src/Rhendaria.Hosting/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Orleans.Configuration;
+using Orleans.Hosting;
+
+namespace Rhendaria.Hosting
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                var host = await StartSilo();
+                Console.WriteLine("Press Enter to terminate...");
+                Console.ReadLine();
+
+                await host.StopAsync();
+
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return;
+            }
+        }
+
+        private static async Task<ISiloHost> StartSilo()
+        {
+            var builder = new SiloHostBuilder()
+                // Use localhost clustering for a single local silo
+                .UseLocalhostClustering()
+
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "MyAwesomeService";
+                })
+                // Configure connectivity
+                .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback);
+                
+                //.ConfigureLogging(logging => logging.AddConsole());
+
+            var host = builder.Build();
+            await host.StartAsync();
+            return host;
+        }
+    }
+}
