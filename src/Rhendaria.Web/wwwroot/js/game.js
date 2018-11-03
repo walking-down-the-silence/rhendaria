8b4104143bbb2db3d19fb64d9e3a6bc3ebfc84c6
Pavlo Hodysh <gaarasmail@gmail.com>
2018-11-03 10:46:12 +0200

    support for pixi.js for rendering the sprites

---
+++ src/Rhendaria.Web/wwwroot/js/game.js
@@ -0,0 +1,6 @@
+﻿let app = new PIXI.Application({ width: 500, height: 500 });
+
+(function () {
+    let gameField = document.getElementById("game-field");
+    gameField.appendChild(app.view);
+})();
