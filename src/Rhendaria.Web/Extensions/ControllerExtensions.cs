648ba477487f0e5e9dd2f220a5b969e9e978e821
yfozekosh <yfozekosh@gmail.com>
2018-11-14 22:18:49 +0200

    added configuration file to host project

---
+++ src/Rhendaria.Web/Extensions/ControllerExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rhendaria.Web.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static string NameOf<TController>(this Controller controller) where TController : Controller
+        {
+            return typeof(TController).Name.Replace(nameof(Controller), string.Empty);
+        }
+    }
+}
