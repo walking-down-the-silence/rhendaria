ffe846faaa53bf827754e7b1b06afac452f4e15d
Pavlo Hodysh <gaarasmail@gmail.com>
2019-02-27 16:42:33 +0200

    managed to load types for js libraries and load types from typescript without using modules

---
+++ src/Rhendaria.Web/Scripts/communication.ts
@@ -0,0 +1,49 @@
+﻿/// <reference types="@aspnet/signalr" />
+
+/**
+ * game communication with backend via websockets
+ */
+let gameCommunication = (function () {
+    let connection = new signalR.HubConnectionBuilder().withUrl("/gameHub").build();
+
+    // event subscriptions from backend server
+    connection.on("UpdatePosition", function (nickname, message) {
+        // TODO: update player position
+        console.log(nickname, message);
+    });
+
+    connection.start().catch((err) => console.error(err));
+
+    // action invocators for frontend client
+    function movePlayer(nickname, position) {
+        connection.invoke("MovePlayer", nickname, position).catch(err => console.error(err));
+    }
+
+    return {
+        movePlayer
+    };
+})();
+
+async function loadGame() {
+    let response = await fetch("pavlo.hodysh", { method: "GET" })
+        .then(result => result.json())
+        .catch(error => console.log(error));
+
+    var zone = Zone.create(
+        Vector.create(response.zone.topLeftX, response.zone.topLeftY),
+        Vector.create(response.zone.bottomRightX, response.zone.bottomRightY));
+    var viewport = Viewport.create(12, 8);
+    var sprites = response.sprites.map(sprite => {
+        let position = Vector.create(sprite.positionX, sprite.positionY);
+        return Sprite.create(sprite.nickname, position);
+    });
+    let player = Player.create(
+        Sprite.create(
+            response.player.nickname,
+            Vector.create(
+                response.player.positionX,
+                response.player.positionY)));
+    let game = Game.create(zone, viewport, player, sprites);
+
+    return game;
+};
---
+++ src/Rhendaria.Web/Scripts/rendering.ts
@@ -0,0 +1,51 @@
+﻿/// <reference path="../node_modules/@types/pixi.js/index.d.ts" />
+
+/**
+ * game view setup and initialization
+ * */
+let gameOptions = {
+    fullWidth: 0,
+    fullHeight: 0
+};
+
+let mouse = {
+    position: null
+};
+
+let app = (async function () {
+    const container = document.getElementById("game-field");
+    gameOptions = {
+        fullWidth: container.offsetWidth,
+        fullHeight: container.offsetHeight
+    };
+
+    const app = new PIXI.Application({
+        antialias: true,
+        autoResize: true,
+        resolution: devicePixelRatio
+    });
+    app.stage.interactive = true;
+    app.renderer.resize(gameOptions.fullWidth, gameOptions.fullHeight);
+    app.view.addEventListener("mousemove", e => {
+        // TODO: get relative coordinates
+        mouse = {
+            position: Vector.create(e.clientX - container.offsetLeft, e.clientY - container.offsetTop)
+        };
+    });
+
+    container.appendChild(app.view);
+
+    // set up a sprite for player in form of a circle
+    const centerX = gameOptions.fullWidth / 2;
+    const centerY = gameOptions.fullHeight / 2;
+
+    //let game = await loadGame();
+    //game.sprites.forEach(sprite => app.stage.addChild(sprite));
+    //app.stage.addChild(game.player.sprite);
+
+    // event subscriptions
+    app.ticker.add(function () { /* move sprites here */ });
+    window.addEventListener("resize", () => app.renderer.resize(window.innerWidth, window.innerHeight));
+
+    return app;
+})();
