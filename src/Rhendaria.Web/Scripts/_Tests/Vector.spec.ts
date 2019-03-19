86c31333defb37080b2192cf4336f251d550e46d
yfozekosh <yfozekosh@gmail.com>
2019-03-19 22:22:18 +0200

    added unit test support on FE
    removed unnecessary folder from web project

---
+++ src/Rhendaria.Web/Scripts/_Tests/Vector.spec.ts
@@ -0,0 +1,45 @@
+﻿import "jasmine";
+import { Vector } from "../Vector";
+
+describe("Vector", () => {
+    it("can be created with 0 0",
+        () => {
+            // Arrange.
+            const x = 0, y = 0;
+
+            // Act.
+            const target = Vector.create(x, y);
+
+            // Assert.
+            expect(target.x).toBe(x);
+            expect(target.y).toBe(y);
+        });
+
+    it("add (0;0),(2;2) result in (2;2)",
+        () => {
+            // Arrange.
+            const target = Vector.create(0, 0);
+            const secondVector = Vector.create(2, 2);
+            const resultX = 2, resultY = 2;
+
+            // Act.
+            const result = target.add(secondVector);
+
+            // Assert.
+            expect(result.x).toBe(resultX);
+            expect(result.y).toBe(resultY);
+        });
+
+    it("shrink (2;2) by 2 result in (1;1)",
+        () => {
+            // Arrange.
+            const target = Vector.create(2, 2);
+
+            // Act.
+            const result = target.shrink(2);
+
+            // Assert.
+            expect(result.x).toBe(1);
+            expect(result.y).toBe(1);
+        });
+});
